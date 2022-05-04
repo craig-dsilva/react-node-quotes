cd4948e2287cef9f5e9fdd2f290bb07b15d98599
craig-dsilva <90406619+craig-dsilva@users.noreply.github.com>
2022-05-04 18:08:25 +0100

    Styling successful

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 const Navigation = () => {
   return (
-    <div>
-      <Link to="/">All Quotes</Link>
-      <Link to="/random">Random Quotes</Link>
+    <div className="Navigation">
+      {/* <Link to="/">All Quotes</Link>
+      <Link to="/random">Random Quotes</Link> */}
+      <Button variant="contained" color="success" component={Link} to="/">
+        All Quotes
+      </Button>
+      <Button variant="contained" color="success" component={Link} to="/random">
+        Random Quotes
+      </Button>
     </div>
   );
 };
